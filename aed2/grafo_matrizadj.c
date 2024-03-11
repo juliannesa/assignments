7b91620d22b2c0dbaa34ef160e438c9b0d64b03f
Julianne Amanda de Sá <91141464+juliannesa@users.noreply.github.com>
2024-03-10 21:21:43 -0300

    Update grafo_matrizadj.c

--- aed2/grafo_matrizadj.c
+++ aed2/grafo_matrizadj.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 #include "grafo_matrizadj.h"
+
+bool inicializaGrafo(Grafo* grafo, int nv){
+  int i,j;
+  if(nv > MAXNUMVERTICES) {
+    fprinf(stderr, "Erro na chamada de inicializaGrafo: numero de vertices maior que o permitido de %d. \n", MAXNUMVERTICES);
+    return false;
+  }
+  if(nv <= 0) {
+    fprinf(stderr, "Erro na chamada de inicializaGrafo: numero de vertices deve ser positivo. \n");
+    return false;
+  }
+  grafo->numVertices = nv;
+  grafo->numArestas = 0;
+  for(i = 0, i < grafo->numVertices; i++){
+    for(j = 0, j < grafo->numVertices; j++){
+      grafo->mat[i][j] = AN;
+    }
+  }
+  return true;
+}
